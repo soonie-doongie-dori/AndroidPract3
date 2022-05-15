ffe5e51848e899330f4af06b059df848007ce8bd
meow <105239123+soonie-doongie-dori@users.noreply.github.com>
2022-05-15 03:17:57 +0300

    pract 3 done

---
+++ practice3/pract3/intentapp/src/main/java/ru/mirea/lukyanchuk/intentapp/SecondActivity.java
@@ -0,0 +1,19 @@
+package ru.mirea.lukyanchuk.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        String text = (String) getIntent().getSerializableExtra("sendText");
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(text);
+    }
+}
